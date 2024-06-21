b85fd6fc7d21f740057e0e917a5eed5360237161
kaylasiqueira2009 <159539899+kaylasiqueira2009@users.noreply.github.com>
2024-06-21 09:12:53 -0300

    adicionando arquivos js e css

--- sketch.js
+++ sketch.js
@@ -1,41 +1,33 @@
 let cor;
-let posicaoHorizontal; // x
-let posicaoVertical; // y
+let circuloX //horizontal
+let circuloY //vertical
+
+let posicaoHorizontal2; // x
+let posicaoVertical2; // y
 
 function setup() {
-  createCanvas(500, 500);
-   background(color(10,0,0));
-  cor = color(random(0,255), random(0,255), random(0,255));
-  posicaoHorizontal = 200;
-  posicaoVertical = 200;
+createCanvas(400, 400);
+background(color(100, 0 ,0));
+  
+cor = color(random(255), random(255), random(255));
+circuloX = [0, 0, 0];
+circuloY = [random(height), random(height), random(height)];
+
 }
-
-
-function draw() {
-  
-  fill(cor);
-  circle(posicaoHorizontal,posicaoVertical,70);
  
+function draw() {
+fill(cor);
+for(let contador in circuloX) {
+circle(circuloX[contador], circuloY[contador], 50);
+circuloX[contador]+= random(0, 3);
+circuloY[contador]+= random(-3, 3);
   
-  
-  if (mouseX < posicaoHorizontal){
-    posicaoHorizontal =  posicaoHorizontal - 1;
-  }
-  
-  if (mouseX > posicaoHorizontal){
-    posicaoHorizontal =  posicaoHorizontal + 1;
-  }
-  
-  if (mouseY < posicaoVertical){
-    posicaoVertical--;
-  }
-    
-  if (mouseY > posicaoVertical){
-    posicaoVertical++;
-  }
-  
-  if (mouseIsPressed){
-    cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
-  }
-    
+if(circuloX[contador] >= width) {
+circuloX[contador] = 0;
+circuloY[contador] = random(height);
+}
+}
+if(mouseIsPressed) {
+cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
+}
 }
